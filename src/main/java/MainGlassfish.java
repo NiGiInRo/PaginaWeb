de4877df16b4becb82fb92b2a470772a63597aa5
nicol <nicol@DESKTOP-JUA9E8M>
2017-11-15 17:08:10 -0500

    paquete y glashfish class

--- src/main/java/MainGlassfish.java
+++ src/main/java/MainGlassfish.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package newpackage;
+
 
 import java.io.File;
 import org.glassfish.embeddable.CommandResult;
